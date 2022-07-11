7c744ba0877188acff6010ca9be2aeda0b86bbaf
Jingrong Chen <crazyboycjr@gmail.com>
2022-07-11 11:53:06 -0700

    add benchmarking scripts for zhen

---
+++ tests/bench_zhen.py
@@ -0,0 +1,287 @@
+import os
+
+import torch
+import numpy as np
+import alpa.torch.optim as torchoptim
+from alpa.torch.trainer import train_torch_module
+import alpa
+from alpa.device_mesh import get_global_cluster
+from alpa import PipeshardParallel
+
+from torch_frontend.test_zhen import ZHENCollection, TokenMixer
+
+
+def weight_init_func(pt_module, name_map, params, bufs):
+    # for k, m in pt_module.named_modules():
+    #     if isinstance(m, torch.nn.Linear):
+    #         params[name_map[f"{k}.weight"]] = torch.nn.init.xavier_uniform(params[name_map[f"{k}.weight"]])
+    #         params[name_map[f"{k}.bias"]] = torch.nn.init.normal(params[name_map[f"{k}.bias"]], std=1e-6)
+    return params, bufs
+
+
+def train_zhen_homogeneous(global_batch_size, num_iters, parallel_method):
+    B = global_batch_size  # 59  # made multiples of 8
+    F = 512
+    D = 160
+    LAYERS = 4
+    OUTPUT_PER_ENSEMBLE = 32  # 50  # made multiples of 8
+    # OUTPUT_PER_ENSEMBLE = 48  # 50  # made multiples of 8
+    # TOKENS = [
+    #     TokenMixer.ATTENTION, TokenMixer.LINEAR, TokenMixer.ATTENTION,
+    #     TokenMixer.CONVOLUTION, TokenMixer.DOT
+    # ]
+    TOKENS = [
+        TokenMixer.ATTENTION, TokenMixer.LINEAR, TokenMixer.ATTENTION,
+        TokenMixer.DOT
+    ]
+
+    pt_module_gen = lambda: ZHENCollection(LAYERS, D, TOKENS, F,
+                                           OUTPUT_PER_ENSEMBLE)
+
+    dataloader = [(torch.empty(
+        B, D, F), torch.empty(B, D * LAYERS * OUTPUT_PER_ENSEMBLE))] * num_iters
+
+    loss_func = lambda *args, **kwargs: torch.nn.functional.mse_loss(
+        *args, **kwargs)
+    optim_gen = torchoptim.adam(lr=1e-3)
+
+    alpa.global_config.xla_client_mem_fraction = 0.7
+
+    train_torch_module(pt_module_gen,
+                       weight_init_func,
+                       dataloader,
+                       loss_func,
+                       optim_gen,
+                       parallel_method)
+
+
+def precheck(pp: int, dp: int, op: int, num_micro_batches: int,
+             num_auto_layers: int, host_ids, device_ids):
+    assert num_auto_layers % pp == 0, f"{num_auto_layers} vs {pp}"
+
+    if host_ids is not None:
+        assert device_ids is not None
+        assert len(host_ids) == pp
+        assert len(device_ids) == pp
+
+    alpa.init("ray")
+    cluster = alpa.device_mesh.get_global_cluster()
+    assert cluster.num_devices == pp * dp * op, f"{cluster.num_devices} vs {pp} * {dp} * {op}"
+
+
+def data_parallel(pp: int, dp: int, op: int, num_micro_batches: int,
+                  num_auto_layers: int) -> PipeshardParallel:
+    precheck(pp, dp, op, num_micro_batches, num_auto_layers)
+
+    cluster = get_global_cluster()
+    return PipeshardParallel(
+        num_micro_batches=num_micro_batches,
+        layer_option=alpa.AutoLayerOption(num_auto_layers),
+        stage_option=alpa.ManualStageOption(
+            forward_stage_layer_ids=np.array_split(range(num_auto_layers), pp),
+            submesh_physical_shapes=[cluster.get_virtual_physical_mesh().shape
+                                    ] * pp,
+            submesh_logical_shapes=[(dp, op)] * pp,
+            submesh_autosharding_option_dicts=[{
+                'force_batch_dim_to_mesh_dim': 0
+            }] * pp),
+        default_auto_sharding_option=alpa.AutoShardingOption(
+            prefer_reduce_scatter=True))
+
+# {
+#     # torchrec: duration of each iteration avg: 0.185473 secs, median: 0.16794457199284807 secs, 90P: 0.16855745000066236 secs, 99P: 1.9236148939817213 secs
+#     # num_micro_batches: 8, duration of each iteration avg: 0.693365 secs, median: 0.15849089622497559 secs, 90P: 0.20190882682800293 secs, 99P: 10.839699506759644 secs
+#     # num_micro_batches: 2, duration of each iteration avg: 0.693497 secs, median: 0.15735721588134766 secs, 90P: 0.1949770450592041 secs, 99P: 10.804256200790405 secs
+#     "description": "data parallel",
+#     "(pp, dp, op)": (1, 8, 1),
+#     "physical_mesh_shape": (1, 8),
+#     "auto_sharding_option": {
+#         'force_batch_dim_to_mesh_dim': 0
+#     },
+# },
+
+PARALLEL_METHODS = [
+    {
+        # num_micro_batches: 32, duration of each iteration avg: 1.116381 secs, median: 1.0280978679656982 secs, 90P: 1.203047752380371 secs, 99P: 3.7230050563812256 secs
+        # num_micro_batches: 16, duration of each iteration avg: 0.705956 secs, median: 0.7422900199890137 secs, 90P: 0.8465251922607422 secs, 99P: 1.3067986965179443 secs
+        # num_micro_batches: 16, using EFA does not help... duration of each iteration avg: 0.738531 secs, median: 0.7759156227111816 secs, 90P: 0.9488911628723145 secs, 99P: 1.2394416332244873 secs
+        # num_micro_batches: 8, duration of each iteration avg: 0.610578 secs, median: 0.6345152854919434 secs, 90P: 0.7277560234069824 secs, 99P: 1.040968894958496 secs
+        "description": "data parallel",
+        "(pp, dp, op)": (1, 16, 1),
+        "physical_mesh_shape": (2, 8),
+        "auto_sharding_option": {
+            'force_batch_dim_to_mesh_dim': 0
+        },
+    },
+    {
+        # incompatible shapes before, removing conv2d bypasses this issue
+        # num_micro_batches: 32, duration of each iteration avg: 5.999120 secs, median: 6.294488430023193 secs, 90P: 7.15912938117981 secs, 99P: 7.891208648681641 secs
+        # num_micro_batches: 16, duration of each iteration avg: 5.624338 secs, median: 5.146093845367432 secs, 90P: 6.106522560119629 secs, 99P: 19.493966579437256 secs
+        # num_micro_batches: 8,
+        "description": "two nodes, within each node running operator parallel",
+        "(pp, dp, op)": (1, 2, 8),
+        "physical_mesh_shape": (2, 8),
+    },
+    {
+        # this plan looks suboptimal, low priority
+        # failed: jax._src.traceback_util.UnfilteredStackTrace: TypeError: lax.dynamic_update_slice requires arguments to have the same dtypes, got bool, uint8.
+        "description":
+            "Example #1: two data pipelines (dp=2), each data pipeline has 4 pipeline stages (pp=4), within each pipeline stages, run tensor parallel on two GPUs.",
+        "(pp, dp, op)": (4, 2, 2),
+        "physical_mesh_shape": (1, 4),
+    },
+    {
+        # failed: never finish the first iteration
+        "description":
+            "Example #2: intra-numa-node (4 gpus) operator parallel, cross-numa and cross-machine pipeline",
+        "(pp, dp, op)": (4, 1, 4),
+        "physical_mesh_shape": (1, 4),
+    },
+    {
+        # This plan looks not bad
+        # duration of each iteration avg: 1.052618 secs, median: 0.760066032409668 secs, 90P: 1.024703025817871 secs, 99P: 6.421065330505371 secs
+        # [2.153844118118286, 2.8738555908203125, 2.2504305839538574]
+        "description": "Example #4: data-parallel across numa-nodes, tensor-parallel within a numa-node",
+        "(pp, dp, op)": (1, 4, 4),
+        "physical_mesh_shape": (2, 8),  # (4, 4) failed: assert required_num_hosts == 1
+    },
+    {
+        # failed: never finish the first iteration
+        "description":
+            "Example #5.1: data parallel + cross-machine pipeline parallel",
+        "(pp, dp, op)": (4, 4, 1),
+        "physical_mesh_shape": (1, 4),
+    },
+    {
+        # This plan looks not bad
+        # num_micro_batches: 32, duration of each iteration avg: 32.164927 secs, median: 31.394895553588867 secs, 90P: 43.208282470703125 secs, 99P: 43.236831188201904 secs
+        # num_micro_batches: 32, run again, duration of each iteration avg: 20.166597 secs, median: 21.410356998443604 secs, 90P: 22.74605417251587 secs, 99P: 22.843191146850586 secs
+        "description": "Example #5.2: data parallel + intra-machine pipeline parallel",
+        "(pp, dp, op)": (4, 4, 1),
+        "physical_mesh_shape": (0, 0),  # (4, 1)
+        # [0,3,2,1], [4,7,6,5]
+        # "host_ids": [[0, 1], [0, 1], [0, 1], [0, 1]],
+        # "device_ids": [[[0, 2], [0, 2]], [[1, 3], [1, 3]], [[4, 6], [4, 6]], [[5, 7], [5, 7]]],
+        # [0,3,4,7], [1,2,5,6]
+        # "host_ids": [[0, 1], [0, 1], [0, 1], [0, 1]],
+        # "device_ids": [[[0, 2], [0, 2]], [[1, 3], [1, 3]], [[4, 6], [4, 6]], [[5, 7], [5, 7]]],
+        # duration of each iteration avg: 20.791150 secs, median: 22.128350257873535 secs, 90P: 23.276580095291138 secs, 99P: 23.45837903022766 secs
+        # 15.52617883682251, 16.176363706588745, 15.91279649734497, 16.444060564041138, 15.856158018112183, 16.74027109146118, 15.548842191696167, 15.776957750320435
+        # [0,1,4,5], [2,3,6,7]
+        # "host_ids": [[0, 1], [0, 1], [0, 1], [0, 1]],
+        # "device_ids": [[[0, 2], [0, 2]], [[1, 3], [1, 3]], [[4, 6], [4, 6]], [[5, 7], [5, 7]]],
+        # 16.703232765197754, 17.657737255096436, 17.30591320991516, 17.8020601272583, 17.031238794326782, 18.052095651626587, 17.76880693435669, 16.9144070148468
+        # another run: 19.64015030860901, 15.822978258132935, 16.193574905395508, 14.976128816604614
+        # [0,1,2,3], [4,5,6,7]
+        "host_ids": [[0, 1], [0, 1], [0, 1], [0, 1]],
+        "device_ids": [[[0, 4], [0, 4]], [[1, 5], [1, 5]], [[2, 6], [2, 6]], [[3, 7], [3, 7]]],
+        # "host_ids": [[0], [1], [0], [1]],
+        # "device_ids": [[[0, 1, 2, 3]], [[0, 1, 2, 3]], [[4, 5, 6, 7]], [[4, 5, 6, 7]]],
+    },
+    {
+        # this looks suboptimal
+        # failed: assert tuple(expected_microbatched_shape) == microbatch_shape, AssertionError: (1, 128) vs (128,)
+        "description":
+            "two gpus a group, each group in charge of a pipeline stage (8 stages)",
+        "(pp, dp, op)": (8, 1, 2),
+        "physical_mesh_shape": (1, 2),
+    },
+]
+
+BENCHMARK_SUITES = {
+    "global_batch_size": 1024 * 16,
+    "num_micro_batches": 16,
+    # actual_batch_per_device = global_batch_size / num_devices
+    "num_iters": 20,
+    "parallel_methods": PARALLEL_METHODS[0:1],
+}
+
+
+def benchmark_zhen_homogeneous(benchmark_case):
+    print(f"benchmarking: {benchmark_case}")
+
+    c = benchmark_case
+    global_batch_size = c["global_batch_size"]
+    num_micro_batches = c["num_micro_batches"]
+    num_iters = c["num_iters"]
+    p = c["parallel_method"]
+
+    # high-level parallelism specification
+    pp, dp, op = p["(pp, dp, op)"]
+    physical_mesh_shape = p.get("physical_mesh_shape", (0, 0))
+    auto_sharding_option = p.get("auto_sharding_option", {})
+    host_ids = p.get("host_ids", None)
+    device_ids = p.get("device_ids", None)
+
+    # num_micro_batches = 32
+    num_auto_layers = pp * 1
+
+    # check
+    precheck(pp, dp, op, num_micro_batches, num_auto_layers, host_ids, device_ids)
+
+    physical_mesh_specs = None
+    if host_ids is not None:
+        physical_mesh_specs = list(zip(host_ids, device_ids))
+
+    # cluster = alpa.device_mesh.get_global_cluster()
+    # virtual_mesh = cluster.get_virtual_physical_mesh()
+    # logical_mesh_shape = (dp, op)
+    # num_mesh_devices = np.prod(logical_mesh_shape)
+    # num_devices_per_host = virtual_mesh.num_devices_per_host
+    # if num_mesh_devices <= num_devices_per_host:
+    #     physical_mesh_shape = (1, num_mesh_devices)
+    # else:
+    #     assert num_mesh_devices % num_devices_per_host == 0
+    #     physical_mesh_shape = (num_mesh_devices // num_devices_per_host,
+    #                            num_devices_per_host)
+
+    # data parallel + cross-mesh pipeline parallel + intra-mesh tensor parallel
+    parallel_method = PipeshardParallel(
+        num_micro_batches=num_micro_batches,
+        layer_option=alpa.AutoLayerOption(num_auto_layers),
+        stage_option=alpa.ManualStageOption(
+            forward_stage_layer_ids=np.array_split(range(num_auto_layers), pp),
+            submesh_physical_shapes=[physical_mesh_shape] * pp,
+            submesh_logical_shapes=[(dp, op)] * pp,
+            submesh_autosharding_option_dicts=[auto_sharding_option] * pp,
+            submesh_physical_spec=physical_mesh_specs),
+        default_auto_sharding_option=alpa.AutoShardingOption(
+            prefer_reduce_scatter=True))
+    # {'force_batch_dim_to_mesh_dim': 0}
+
+    # data parallel
+    # parallel_method = data_parallel(pp, dp, op, num_micro_batches,
+    #                                 num_auto_layers)
+
+    # Result forward_stage_layer_ids: [[0], [1]]
+    # Result meshes: [(1, 8), (1, 8)]
+    # Result logical_mesh_shapes: [(1, 8), (1, 8)]
+    # Result autosharding_option_dicts: [{}, {}]
+    # auto search
+    # parallel_method = alpa.PipeshardParallel(
+    #     stage_mode="auto", num_micro_batches=num_micro_batches)
+
+    # faster auto search
+    # parallel_method = PipeshardParallel(
+    #     stage_mode="uniform",
+    #     num_micro_batches=num_micro_batches,
+    #     layer_option=alpa.AutoLayerOption(num_auto_layers, remat_layer=True),
+    #     default_auto_sharding_option=alpa.AutoShardingOption(
+    #         force_data_parallel=True))
+
+    train_zhen_homogeneous(global_batch_size, num_iters, parallel_method)
+
+
+def main():
+    for parallel_method in BENCHMARK_SUITES["parallel_methods"]:
+        case = BENCHMARK_SUITES.copy()
+        del case["parallel_methods"]
+        case["parallel_method"] = parallel_method
+        benchmark_zhen_homogeneous(case)
+
+
+if __name__ == '__main__':
+    # os.environ["NVIDIA_TF32_OVERRIDE"] = "0"
+    os.environ["NCCL_DEBUG"] = "info"
+    os.environ["ALPA_USE_AWS_EFA"] = "1"
+    main()
---
+++ tests/zhen_torchrec.py
@@ -0,0 +1,146 @@
+from enum import Enum
+from typing import List, Optional, Tuple, Union, Callable
+from collections import namedtuple
+import logging
+import time
+
+import torch
+import torch.nn as nn
+import numpy as np
+import torch.distributed as dist
+from torch.nn.parallel import DistributedDataParallel
+
+from torch_frontend.test_zhen import ZHENCollection, TokenMixer
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    level=logging.INFO,
+    format=
+    '%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+)
+
+# A tuple to wrap all training states.
+TrainState = namedtuple("TrainState", ["params", "optim_state"])
+
+
+class TrainWrapper(nn.Module):
+    """nn.Module to wrap model to use with train_pipeline."""
+
+    def __init__(self, module: nn.Module, loss_func) -> None:
+        super().__init__()
+        self.model = module
+        self.loss_func = loss_func
+
+    def forward(self, batch) -> torch.Tensor:
+        inputs, targets = batch
+        out = self.model(inputs)
+        return self.loss_func(out, targets)
+
+
+# Define one gradient descent step
+def train_step(model, optim, batch):
+    # compute loss
+    loss = model(batch)
+
+    # compute gradient
+    loss.backward()
+
+    # do optimizer step
+    optim.step()
+
+    return loss
+
+
+def train(model, dataloader, optim):
+    latencies = []
+
+    # Run training loops
+    sstart = time.perf_counter()
+    for i, batch in enumerate(dataloader):
+        start = time.perf_counter()
+        loss_value = train_step(model, optim, batch)
+        # torch.cuda.synchronize()
+        end = time.perf_counter()
+        latencies.append(end - start)
+
+        # do whatever with the loss value, e.g. plot it on a graph
+        logger.info(f"Iter: {i}, Loss: {float(loss_value):.6f}")
+    eend = time.perf_counter()
+
+    print('avg:', (eend - sstart) / len(dataloader))
+    latencies.sort()
+    avg_s = sum(latencies) / len(dataloader)
+    logger.info(f"duration of each iteration avg: {avg_s:.6f} secs, "
+                f"median: {latencies[int(len(latencies) * 0.5)]} secs, "
+                f"90P: {latencies[int(len(latencies) * 0.9)]} secs, "
+                f"99P: {latencies[int(len(latencies) * 0.99)]} secs")
+
+
+def train_zhen_homogeneous(batch_size_per_device, num_iters):
+    B = batch_size_per_device
+    F = 512
+    D = 160
+    LAYERS = 4
+    OUTPUT_PER_ENSEMBLE = 32
+    TOKENS = [
+        TokenMixer.ATTENTION, TokenMixer.LINEAR, TokenMixer.ATTENTION,
+        TokenMixer.DOT
+    ]
+    # B = batch_size_per_device
+    # F = 37
+    # D = 160
+    # LAYERS = 3
+    # OUTPUT_PER_ENSEMBLE = 48
+    # TOKENS = [[TokenMixer.ATTENTION, TokenMixer.DOT],
+    #       [
+    #           TokenMixer.ATTENTION, TokenMixer.CONVOLUTION,
+    #           TokenMixer.DOT
+    #       ], [TokenMixer.ATTENTION, TokenMixer.DOT]]  # 3-layer ZHEN
+
+    # The flag below controls whether to allow TF32 on matmul. This flag defaults to False
+    # in PyTorch 1.12 and later.
+    # torch.backends.cuda.matmul.allow_tf32 = True
+
+    # The flag below controls whether to allow TF32 on cuDNN. This flag defaults to True.
+    # torch.backends.cudnn.allow_tf32 = True
+
+    zhen_module = ZHENCollection(LAYERS, D, TOKENS, F, OUTPUT_PER_ENSEMBLE)
+
+    dataloader = [(torch.empty(
+        B, D, F).cuda(), torch.empty(B, D * LAYERS * OUTPUT_PER_ENSEMBLE).cuda())] * num_iters
+
+    loss_func = lambda *args, **kwargs: torch.nn.functional.mse_loss(
+        *args, **kwargs)
+
+    dist.init_process_group("nccl")
+    rank = dist.get_rank()
+    train_model = TrainWrapper(zhen_module, loss_func).to(rank % 8)
+
+    model = DistributedDataParallel(train_model, device_ids=[rank % 8])
+    # model = train_model.to('cuda:0')
+
+    print(model)
+    optim = torch.optim.Adam(list(model.parameters()), lr=1e-3)
+    train(model, dataloader, optim)
+
+
+SETTING = {
+    "batch_size_per_device": 1024,
+    "num_iters": 100,
+}
+
+def main():
+    batch_size_per_device = SETTING["batch_size_per_device"]
+    num_iters = SETTING["num_iters"]
+    train_zhen_homogeneous(batch_size_per_device, num_iters)
+
+
+if __name__ == '__main__':
+    import os
+    os.environ["FI_PROVIDER"] = "efa"
+    os.environ["FI_EFA_USE_DEVICE_RDMA"] = "1"
+    os.environ["NCCL_DEBUG"] = "info"
+    os.environ["LD_LIBRARY_PATH"] = os.getenv("LD_LIBRARY_PATH", "")
+    print(os.environ)
+    main()
